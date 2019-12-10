d43538f26181afea71b8b5335414498b24636944
Ian Andrewson <ianandrewson@gmail.com>
2019-12-10 12:47:13 -0800

    doubly linked list implemented, all tests pass

--- challenges/linkedList/doubly-linked-list.test.js
+++ challenges/linkedList/doubly-linked-list.test.js
@@ -1,14 +1,24 @@
-const { LinkedList } = require('../linkedList/linked-list');
+const { DoublyLinkedList } = require('./doubly-linked-list');
 
-describe('linked-list-insertions tests', () => {
+describe('doubly-linked-list tests', () => {
+  it('should have a previous node', () => {
+    const list = new DoublyLinkedList();
+    list.insert(1);
+    expect(list.head.next).toBeNull;
+    expect(list.head.previous).toBeNull;
+    list.append(2);
+    expect(list.head.next.value).toEqual(2);
+    expect(list.head.previous).toBeNull;
+    expect(list.head.next.previous.value).toBe(1);
+  });
   it('can successfully add a node to the end of the linked list', () => {
-    const list = new LinkedList();
+    const list = new DoublyLinkedList();
     list.insert(5);
     list.append(6);
-    expect(list.current.next).toEqual({ value: 6, next: null });
+    expect(list.head.next.value).toEqual(6);
   });
   it('can successfully add multiple nodes to the end of a linked list', () =>{
-    const list = new LinkedList();
+    const list = new DoublyLinkedList();
     list.insert(1);
     list.append(2);
     list.append(3);
@@ -17,7 +27,7 @@ describe('linked-list-insertions tests', () => {
     expect(list.head.next.next.value).toEqual(3);
   });
   it('can successfully insert a node before a node located in the middle of a linked list', () => {
-    const list = new LinkedList();
+    const list = new DoublyLinkedList();
     list.insert(1);
     list.append(3);
     list.append(4);
@@ -29,7 +39,7 @@ describe('linked-list-insertions tests', () => {
     expect(() => list.insertBefore(99, 'Not Found')).toThrowError(new Error('Value to insert before was not found.'));
   });
   it('can successfully insert a node before the first node of a linked list', () => {
-    const list = new LinkedList();
+    const list = new DoublyLinkedList();
     list.insert(2);
     list.insert(1);
     list.insert(0);
@@ -37,7 +47,7 @@ describe('linked-list-insertions tests', () => {
     expect(list.head.next.value).toEqual(1);
   });
   it('can successfully insert after a node in the middle of the linked list', () => {
-    const list = new LinkedList();
+    const list = new DoublyLinkedList();
     list.insert(1);
     list.append(3);
     list.insertAfter(1, 2);
@@ -47,7 +57,7 @@ describe('linked-list-insertions tests', () => {
     expect(() => list.insertAfter(99, 'NOPE')).toThrowError(new Error('Value to insert after was not found.'));
   });
   it('can successfully insert after the last node of the linked list', () => {
-    const list = new LinkedList();
+    const list = new DoublyLinkedList();
     list.insert(1);
     list.insertAfter(1, 2);
     list.insertAfter(2, 3);
@@ -57,12 +67,19 @@ describe('linked-list-insertions tests', () => {
     expect(list.head.next.next.next).toBeNull();
   });
   it('can successfully delete a node base on a value', () => {
-    const list = new LinkedList();
+    const list = new DoublyLinkedList();
     list.insert(1);
     list.append(2);
     list.append(3);
     list.delete(2);
     expect(list.head.value).toEqual(1);
     expect(list.head.next.value).toEqual(3);
+  });
+  it('can string all values of the linked list together', () => {
+    const list = new DoublyLinkedList();
+    list.insert(2);
+    list.append(4);
+    list.insertBefore(4, 3);
+    expect(list.toString()).toEqual('2 3 4');
   });
 });
