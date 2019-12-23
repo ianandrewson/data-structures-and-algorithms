09bc5bd6286718a71975aebe42a6387fa1dd3807
Ian Andrewson <ianandrewson@gmail.com>
2019-12-22 18:42:24 -0800

    stack.push() implemented, test passes.

--- challenges/queueWithStacks/queue-with-stacks.js
+++ challenges/queueWithStacks/queue-with-stacks.js
@@ -1,12 +1,26 @@
 class Node {
   constructor(val){
-    this.val = val;
+    this.value = val;
     this.next = null;
   }
 }
 
 class Stack {
-  
+  constructor(){
+    this.head = null;
+  }
+
+  push(val){
+    let current = this.head;
+    this.head = new Node(val);
+    this.head.next = current;
+  }
+
+  pop(){
+    const toReturn = this.head.value;
+    this.head = this.head.next;
+    return toReturn;
+  }
 }
 
 class Queue {
